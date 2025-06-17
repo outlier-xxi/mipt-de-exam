fb0e9881f9f4af3104249078cf4442e90be60832
Valeriy Ernandes <lev.370x@gmail.com>
2025-06-17 23:06:12 +0200

    update

--- src/etl/fetch.py
+++ src/etl/fetch.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 import requests
 from src.common.log import logger
 from src.common.settings import settings
@@ -7,8 +9,8 @@ from src.common.settings import settings
 def main():
     logger.info(tmsg := f"fetcher ...")
 
-    data_dir = settings.data_dir
-    filename: str = f"{data_dir}/wbdc.csv"
+    data_dir: str = settings.data_dir
+    filename: str = settings.data_file
     url: str = settings.url
 
     os.makedirs(data_dir, exist_ok=True)
@@ -19,10 +21,18 @@ def main():
             response = session.get(url)
             response.raise_for_status()
             logger.info(f"Response: {response.status_code}")
+            logger.info(f"{msg} done")
             if response.status_code == 200:
+                logger.info(f"Writing to {filename} ...")
                 file.write(response.text)
+                logger.info(f"{msg} done")
+            else:
+                raise AssertionError(f"Failed to get: {url}")
+    else:
+        logger.info(f"File {filename} already exists. Skipping")
+        sys.exit(99)
 
-        logger.info(f"{tmsg} done")
+    logger.info(f"{tmsg} done")
 
 
 if __name__ == '__main__':
